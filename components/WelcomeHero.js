a3e6c87dd2a8067b329ddaf2b41aa9f67ca81b77
hmjabs7 <124741935+hmjabs7@users.noreply.github.com>
2023-02-13 07:24:18 +0600

    Update WelcomeHero.js

--- components/WelcomeHero.js
+++ components/WelcomeHero.js
@@ -10,8 +10,7 @@ export default function WelcomeHero() {
                         <div className="py-7 flex">
                             <div>
                                 <h1 className="text-slate-800 text-xl sm:text-2xl md:text-3xl font-bold">
-                                    <img src="/main/E1.jpg" width="500" height="600"/>
-                                        <link rel="icon" href="/favicon.ico" />
+                                    <img src="main/E1.jpg" width="500" height="600"/>
                                     Welcome to University Managment System
                                 </h1>
                             </div>
